751323013fb251cbe45fcf023367c87b7258d162
Edgar Silva <edgarasdaa10@gmail.com>
2023-02-18 16:53:23 +0000

    Level UI.

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,18 @@
-use bevy::prelude::{Query, ResMut, With};
 use bevy::prelude::{AssetServer, Res};
+use bevy::prelude::{Query, ResMut, With};
+use bevy_egui::egui::{Image, Rect, RichText};
 use bevy_egui::{egui, EguiContext};
-use bevy_egui::egui::{Image, Rect};
 
+use crate::level::LevelResource;
 use crate::player::Player;
 use crate::stats::Health;
 
-pub fn draw_hud(mut egui_context: ResMut<EguiContext>, asset_server: Res<AssetServer>, query: Query<&Health, With<Player>>) {
+pub fn draw_hud(
+    mut egui_context: ResMut<EguiContext>,
+    asset_server: Res<AssetServer>,
+    query: Query<&Health, With<Player>>,
+    level: Res<LevelResource>,
+) {
     let health_bar_fill = egui_context.add_image(asset_server.load("health_bar_fill.png"));
     let health_bar_border = egui_context.add_image(asset_server.load("health_bar_border.png"));
 
@@ -33,8 +39,16 @@ pub fn draw_hud(mut egui_context: ResMut<EguiContext>, asset_server: Res<AssetSe
                     },
                     Image::new(health_bar_fill, size),
                 );
-
             });
 
+        egui::SidePanel::right("right_panel")
+            .frame(egui::Frame::none())
+            .exact_width(110.)
+            .resizable(false)
+            .show_separator_line(false)
+            .show(egui_context.ctx_mut(), |ui| {
+                ui.add_space(8.);
+                ui.label(RichText::new(format!("Level: {}", level.level)).size(24.));
+            });
     }
 }
